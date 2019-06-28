9a8da3be7a78b3e7e863f74532f3838c6f5570e7
Sarah Kaplan <sarahkaplan@Sarahs-MBP-2.lan>
2019-06-27 21:23:45 -0400

    initial

---
+++ datastore.js
@@ -0,0 +1,122 @@
+const STORE = [
+  {
+    question: "Who of the following is not a wizard?",
+    answers: ["Radagast", "Gandalf", "Denethor", "Saruman"],
+    correctAnswer: "Denethor",
+    icon:
+      "https://s3-us-west-2.amazonaws.com/files.catholicworldreport.com/2018/04/4057Denethorjpg_00000003512-575x381.jpg",
+    alt: "Denethor"
+  },
+  {
+    question: "What is an Ent?",
+    answers: [
+      "A Shepherd of the Trees",
+      "An Elf-Lord",
+      "A Female Elf",
+      "A Tree"
+    ],
+    correctAnswer: "A Shepherd of the Trees",
+    icon:
+      "https://deathwatchminis.files.wordpress.com/2019/01/cc8a2252bf7aa0e5e047c0e935d7b390.jpg?w=1040",
+    alt: "Treebeard, an Ent"
+  },
+
+  {
+    question: "How did the Fellowship cross the Misty Mountains?",
+    answers: [
+      "At the Gap of Rohan",
+      "Through the Mines of Moria",
+      "By the Pass of Caradhras",
+      "By the Stairs of Cirith Ungol"
+    ],
+    correctAnswer: "Through the Mines of Moria",
+    icon: "https://lotro-wiki.com/images/1/10/Moria.jpg",
+    alt: "The Mines of Moria"
+  },
+
+  {
+    question: "While in Mordor, Frodo and Sam ate:",
+    answers: [
+      "The athelas plant",
+      "Stewed rabbit",
+      "Elvish lembas bread",
+      "Nothing"
+    ],
+    correctAnswer: "Elvish lembas bread",
+    icon: "https://i.gifer.com/KQSy.gif",
+    alt: "Lembas Bread gif"
+  },
+
+  {
+    question: "When did Gollum first see Frodo?",
+    answers: [
+      "At the Falls of Rauros",
+      "In the Mines of Moria",
+      "In the Emyn Muil",
+      "In the country of Hollin"
+    ],
+    correctAnswer: "In the Mines of Moria",
+    icon:
+      "https://i.pinimg.com/originals/7e/9d/59/7e9d59cada0cbda752a06e28913e9dd1.gif",
+    alt: "Gollum in the mines gif"
+  },
+
+  {
+    question: "Who is the father of Aragorn?",
+    answers: ["Elrond", "Elendil", "Arathorn", "Isildur"],
+    correctAnswer: "Arathorn",
+    icon:
+      "https://memegenerator.net/img/instances/75337730/i-am-aragorn-son-of-arathorn.jpg",
+    alt: "Aragorn, Son of Arathorn meme"
+  },
+
+  {
+    question: "What did Frodo see in the Mirrormere of Kheled-zâram?",
+    answers: [
+      "The stars of Durin",
+      "His own reflection",
+      "The Eye of Sauron",
+      "Trees being felled in Bywater"
+    ],
+    correctAnswer: "The stars of Durin",
+    icon:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/58d67ab0-b782-40bc-b028-e12d55780948/d9rdod9-97bf5568-8939-468c-8fa2-e2949b2171ff.png/v1/fill/w_778,h_1028,strp/durin_s_crown_by_seraphimcrystal_d9rdod9-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTM1MyIsInBhdGgiOiJcL2ZcLzU4ZDY3YWIwLWI3ODItNDBiYy1iMDI4LWUxMmQ1NTc4MDk0OFwvZDlyZG9kOS05N2JmNTU2OC04OTM5LTQ2OGMtOGZhMi1lMjk0OWIyMTcxZmYucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.8kXALo_mt1k_mbtmzjGbpf5LzijRLFxMY2h_UxrAdlQ",
+    alt: "the stars of durin"
+  },
+
+  {
+    question: "When did Samwise leave Frodos side?",
+    answers: [
+      "Never",
+      "On the Stairs of Cirith Ungol",
+      "After he was poisoned by Shelob",
+      "On the slopes of Mount Doom"
+    ],
+    correctAnswer: "After he was poisoned by Shelob",
+    icon: "https://i.makeagif.com/media/8-26-2015/oiasrM.gif",
+    alt: "Sam and Shelob gif"
+  },
+
+  {
+    question: "Who is Sauron?",
+    answers: [
+      "One of the Valar",
+      "One of the Maiar",
+      "One of the Noldor",
+      "A balrog of Morgoth"
+    ],
+    correctAnswer: "One of the Maiar",
+    icon:
+      "http://66.media.tumblr.com/1812527268102ad391033056d9a87cfc/tumblr_mrwp2uMCIU1sx2peuo1_500.gif",
+    alt: "Sauron gif"
+  },
+
+  {
+    question: 'What is the Elvish word for "friend"?',
+    answers: ['"Namárieë"', '"Noro lim"', '"Mallorn"', '"Mellon"'],
+    correctAnswer: '"Mellon"',
+    icon:
+      "https://i.pinimg.com/originals/ec/45/16/ec45167a465130a9901c9b8910932c4d.jpg",
+    alt: "speak friend and enter"
+  }
+];
---
+++ index.js
@@ -0,0 +1,154 @@
+let questionNumber = 0;
+let score = 0;
+
+function generateQuestion() {
+  if (questionNumber < STORE.length) {
+    return `<div class="question-${questionNumber}">
+    <h2>${STORE[questionNumber].question}</h2>
+    <form>
+    <fieldset>
+    <label class="answerOption">
+    <input type="radio" value="${
+      STORE[questionNumber].answers[0]
+    }" name="answer" required>
+    <span>${STORE[questionNumber].answers[0]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${
+      STORE[questionNumber].answers[1]
+    }" name="answer" required>
+    <span>${STORE[questionNumber].answers[1]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${
+      STORE[questionNumber].answers[2]
+    }" name="answer" required>
+    <span>${STORE[questionNumber].answers[2]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${
+      STORE[questionNumber].answers[3]
+    }" name="answer" required>
+    <span>${STORE[questionNumber].answers[3]}</span>
+    </label>
+    <button type="submit" class="submitButton">Submit</button>
+    </fieldset>
+    </form>
+    </div>`;
+  } else {
+    renderResults();
+    restartQuiz();
+    $(".questionNumber").text(10);
+  }
+}
+
+function changeQuestionNumber() {
+  //if (questionNumber < STORE.length) {
+  questionNumber++;
+  //}
+  $(".questionNumber").text(questionNumber + 1);
+}
+
+function changeScore() {
+  score++;
+}
+
+function beginTheQuiz() {
+  $(".beginQuiz").on("click", ".beginButton", function(event) {
+    $(".beginQuiz").remove();
+    $(".questionAndAnswerForm").css("display", "block");
+    $(".questionNumber").text(1);
+  });
+}
+
+function renderQuestion() {
+  $(".questionAndAnswerForm").html(generateQuestion());
+}
+
+function userSelectAnswer() {
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    let selected = $("input:checked");
+    let answer = selected.val();
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+    if (answer === correctAnswer) {
+      selected.parent().addClass("correct");
+      ifAnswerIsCorrect();
+    } else {
+      selected.parent().addClass("wrong");
+      ifAnswerIsWrong();
+    }
+  });
+}
+
+function ifAnswerIsCorrect() {
+  userAnswerFeedbackCorrect();
+  updateScore();
+}
+
+function ifAnswerIsWrong() {
+  userAnswerFeedbackWrong();
+}
+
+function userAnswerFeedbackCorrect() {
+  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+  $(".questionAndAnswerForm").html(
+    `<div class="correctFeedback"><div class="icon"><img src="${
+      STORE[questionNumber].icon
+    }" alt="${
+      STORE[questionNumber].alt
+    }"/></div><p><b>Correct! I suppose you think that was terribly clever...</b></p><button type=button class="nextButton">Go forth</button></div>`
+  );
+}
+
+function userAnswerFeedbackWrong() {
+  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+  // let iconImage = `${STORE[questionNumber].icon}`;
+  $(".questionAndAnswerForm").html(
+    `<div class="correctFeedback"><div class="icon"><img src="${
+      STORE[questionNumber].icon
+    }" alt="${
+      STORE[questionNumber].alt
+    }"/></div><p><b>Fool of a Took! Your answer has been cast back into the fiery chasm from whence it came...</b><br>The correct answer is <span>"${correctAnswer}"</span></p><button type=button class="nextButton">Go forth</button></div>`
+  );
+}
+
+function updateScore() {
+  changeScore();
+  $(".score").text(score);
+}
+
+function renderResults() {
+  if (score >= 7) {
+    $(".questionAndAnswerForm").html(
+      `<div class="results correctFeedback"><h3>Behold! A true Tolkienite, indeed!</h3><p>You scored: ${score} / 10</p><p>Here at last, dear friends, on the shores of the Sea comes the end of our quiz! Go in peace!</p><button class="restartButton">Restart Quiz</button></div>`
+    );
+  } else {
+    $(".questionAndAnswerForm").html(
+      `<div class="results correctFeedback"><h3>YOU SHALL NOT PASS! But do not weep young hobbit, for not all tears are an evil...</h3><p>You scored: ${score} / 10</p><p>Here at last, dear friends, on the shores of the Sea comes the end of our quiz! Go in peace!</p><button class="restartButton">Restart Quiz</button></div>`
+    );
+  }
+}
+
+function renderNextQuestion() {
+  $("main").on("click", ".nextButton", function(event) {
+    changeQuestionNumber();
+    renderQuestion();
+    userSelectAnswer();
+  });
+}
+
+function restartQuiz() {
+  $("main").on("click", ".restartButton", function(event) {
+    location.reload();
+  });
+}
+
+function createQuiz() {
+  beginTheQuiz();
+  renderQuestion();
+  userSelectAnswer();
+  renderNextQuestion();
+}
+
+$(createQuiz);
